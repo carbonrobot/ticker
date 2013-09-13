f90f0a36385d4d205b796f50a12756291c378fc1
Charles Brown <carbonrobot@gmail.com>
2013-09-12 22:48:27 -0500

    Initial commit

---
+++ server.js
@@ -0,0 +1,40 @@
+var jade = require('jade'),
+	express = require('express'),
+	app = express(),
+    server = require('http').createServer(app),
+	io = require('socket.io').listen(server);
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+
+app.use(express.logger());
+app.use(express.compress());
+app.use(express.static(__dirname + '/public'));
+	
+app.get('/', function(req, res){
+	res.render('index');
+});
+
+server.listen(process.env.PORT || 4561);
+console.log('listening on' + process.env.PORT || 4561);
+
+var cp = require('child_process').fork('ticker');
+cp.on('message', function (message) {
+    io.sockets.emit('update', message);
+});
+
+io.sockets.on('connection', function (socket) {
+    socket.emit('status', { message: "EHLO OK Connected" });
+    socket.on('start', function (data) {
+        cp.send({ op: 'start' });
+        socket.emit('status', { message: "Processing" });
+    });
+    socket.on('stop', function (data) {
+        cp.send({ op: 'stop' });
+        socket.emit('status', { message: "Stopped" });
+    });
+    socket.on('buy', function (data) {
+        cp.send({ op: 'buy', name: data });
+        socket.emit('status', { message: "Stopped" });
+    });
+});
